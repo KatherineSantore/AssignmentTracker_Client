fe768bd3857ee6768507706981d8685ab3469c88
KatherineWard <katherine.eileen.ward@gmail.com>
2018-08-05 22:50:07 -0400

    added modals and jquery functionality to index.html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,7 +5,25 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const authEvents = require('./auth/events')
+// const assignmentEvents = require('./assignment/events')
+// const assignmentUi = require('./assignment/ui')
+const authUi = require('./auth/ui')
 
 $(() => {
-  // your JS code goes here
+  // Authentication Event Handler
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePW)
+  $('#sign-out').on('click', authEvents.onSignOut)
+
+  // Item Events
+  // $('#create-item-form').on('submit', assignmentEvents.onCreateItem)
+
+  // Handlebar Event Handlers
+  // assignmentEvents.addHandlers()
+
+  // Reset UI Handling Elements
+  // $('.content-button').on('click', assignmentUi.resetUiHandleing)
+  $('.menu').on('click', authUi.resetAuth)
 })
