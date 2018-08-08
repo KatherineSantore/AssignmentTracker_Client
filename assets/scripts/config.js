bfb86f36a4a41ae4c7b351d3e216edf918534382
KatherineWard <katherine.eileen.ward@gmail.com>
2018-08-08 11:29:20 -0400

    took space away from URL in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://assignmenttracker.herokuapp.com ',
+  production: 'https://assignmenttracker.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
